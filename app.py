f71e853d37a9c98ce7f5766e8e9fdbcda24c80a3
Bruno Molina <145416675+brumab@users.noreply.github.com>
2025-12-26 19:31:51 -0300

    Update MySQL configuration to use double quotes

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
-app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
-app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
-app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
-app.config['MYSQL_PORT'] = int(os.environ.get('MYSQL_PORT', 3306))
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-app.config['MYSQL_SSL'] = {'ssl-mode': 'REQUIRED'}
+app.config['MYSQL_HOST'] = os.environ.get("MYSQL_HOST")
+app.config['MYSQL_USER'] = os.environ.get("MYSQL_USER")
+app.config['MYSQL_PASSWORD'] = os.environ.get("MYSQL_PASSWORD")
+app.config['MYSQL_DB'] = os.environ.get("MYSQL_DB")
+app.config['MYSQL_PORT'] = int(os.environ.get("MYSQL_PORT", 3306))
+app.config['MYSQL_CURSORCLASS'] = "DictCursor"
+app.config['MYSQL_SSL'] = {"ssl": {}}
+
 
 mysql = MySQL(app)
 
